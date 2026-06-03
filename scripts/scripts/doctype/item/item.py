7b2deb54308f35b2324455bf586f4f034482ca89
Molnár Botond <45335158+Gepsonka@users.noreply.github.com>
2026-06-03 15:43:27 +0000

    updated naming of items

--- scripts/scripts/doctype/item/item.py
+++ scripts/scripts/doctype/item/item.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2026, asd and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.model.naming import make_autoname
+from frappe.utils import strip
 
 
 class Item(Document):
-	pass
+	def autoname(self):
+		if not self.item_code:
+			series = self.naming_series or ".######"
+			self.item_code = make_autoname(series, doc=self)
+
+		self.item_code = strip(self.item_code)
+		self.name = self.item_code
+
+	def validate(self):
+		if not self.item_name:
+			self.item_name = self.item_code
